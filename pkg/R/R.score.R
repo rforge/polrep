e3c7c9bf5e9013f0b9f313b5fd7e72f5b20922aa
ruedin <ruedin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-04 21:46:15 +0000

    can specify k in relrep
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/polrep@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/R.score.R
+++ pkg/R/R.score.R
@@ -1,8 +1,11 @@
-R.score <-
-function(Z,R) {
-  # (assumes majority group in position 1)
-  # z <- length(Z)
-  # r <- length(R)
-  # sum(R[2:r]) / sum(Z[2:z])
-  sum(R[-1]) / sum(Z[-1])
+relrep <- R.score <-
+function(Z, R, k=1) {
+  # k as the position of the group under study (e.g. majority group)
+  # no input validation, so that we can use ad-hoc combinations like k=1:3
+  # if k is less than 1 or greater than the number of groups, set it to 1 (invalid input)
+  # if (k>length(Z) | k<1) {
+  #   warning("Warning: Invalid group, assumes group in position 1")
+  #  k <- 1
+  # }
+  sum(R[-k]) / sum(Z[-k])
   }
