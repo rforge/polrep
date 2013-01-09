76d2053b758565d27223e84f41b7212f8cdafc39
ruedin <ruedin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-09 08:42:33 +0000

    0.18 neff for incomplete; req. agrmt
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/polrep@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/neffIncomp.R
@@ -0,0 +1,17 @@
+neffIncomp <- function (x) {
+  # effetive number of parties for incomplete data
+  # assumes residual category to be the last item!
+  P <- sum(x)                       # number of seats
+  R <- x[length(x)]                 # last item = residual category (assumed!)
+  A <- x[1:length(x)-1]             # exclude last item ( = exclude residual)
+  PL <- minnz(A)                    # smallest non-zero component reported
+  # requires minnz() from agrmt package (non-zero minimum)
+  noN <- P^2/(R + sum(A^2))         # f(R) = R, residual not squared ("simple")
+  lowN <- P^2/(R^2 + sum(A^2))      # f(R) = R^2 ( = residual included)
+  highN <- P^2/(sum(A^2))           # f(R) = 0, residual excluded
+  leastN <- P^2/(R*PL + sum(A^2))   # f(R) = least component
+  M <- ifelse (min(R^2, R*PL) == R*PL, leastN, lowN) # lower of R ^2 and PL
+  B <- mean(c(highN,M))             # best estimate
+  r <- list(Neff = B,  simple = noN, low = lowN, least = leastN, high = highN)
+  return(r)
+}
