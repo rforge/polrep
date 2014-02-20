429b130d490aeb0a6125eb759f7564b2438384c9
ruedin <ruedin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-20 19:41:15 +0000

    0.26 thicker lines in IRS plot; correct cross-reference
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/polrep@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/irs.plot.R
+++ pkg/R/irs.plot.R
@@ -9,14 +9,14 @@ irs.plot <- function(G, H, perspective="individual", groups=c("Voters","MP"), co
   split.screen(c(2,1))           # screens 1 and 2
   split.screen(c(1,2), screen=1) # screens 3 and 4 (replacing screen 1)
     screen(3)
-      plot(G, type="h", axes=FALSE, ylab=groups[1], xlab="")
+      plot(G, type="h", axes=FALSE, ylab=groups[1], xlab="", lwd=2)
       axis(1); axis(2)
     screen(4)
-      plot(H, type="h", axes=FALSE, ylab=groups[2], xlab="")
+      plot(H, type="h", axes=FALSE, ylab=groups[2], xlab="", lwd=2)
       axis(1); axis(2)
     screen(2)
       par(mar=c(4,4,2,3)) # borders
-      plot(irs.cat(G,H,perspective=perspective), type="h", axes=FALSE, ylab="IRS", xlab=concept)
+      plot(irs.cat(G,H,perspective=perspective), type="h", axes=FALSE, ylab="IRS", xlab=concept, lwd=2)
       axis(1); axis(2)
       abline(h=0, col="grey")
   close.screen(all.screens=TRUE)
