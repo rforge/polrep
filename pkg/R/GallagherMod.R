833daac86ea9681d4858766c678169c17cb5ee2b
ruedin <ruedin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-12-07 10:07:29 +0000

    0.31.4 non-ASCII in comment; namespace
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/polrep@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/GallagherMod.R
+++ pkg/R/GallagherMod.R
@@ -1,9 +1,9 @@
 GallagherMod <- function(Z, R) {
 # modified Gallagher index
-	# x’=x/sum(xi²)^.5
+	# x'=x/sum(xi^2)^.5
 	Z.div <- sum(Z^2)^0.5
 	Z.mod <- Z/Z.div
-	# y’=y/sum(yi²)^.5
+	# y'=y/sum(yi^2)^.5
 	R.div <- sum(R^2)^0.5
 	R.mod <- R/R.div
 	sqrt(sum((Z.mod-R.mod)^2)/2)
